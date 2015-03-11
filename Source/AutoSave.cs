1819c561f93c9e8f55d0985448ae24dfc94f05f4
Palmer11 <jordie_796@hotmail.com>
2015-03-10 21:19:22 -0400

    Initial Commit
    Cities: Skylines Mod. Auto Save your games progress every 10 minutes by
    default.

---
+++ Source/AutoSave.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using ICities;
+using UnityEngine;
+
+
+namespace Mod.AutoSave
+{
+
+    public class AutoSave : IUserMod 
+    {
+
+        public string Name 
+        {
+            get { return "AutoSave"; }
+        }
+
+        public string Description 
+        {
+            get { return "Auto Saves your current game every 10 minutes."; }
+        }
+    }
+
+    public class ThreadingAutoSave : ThreadingExtensionBase
+    {
+        float time = 0.0f;
+        float delay = 600.0f;
+
+        public override void OnUpdate(float realTimeDelta, float simulationTimeDelta)
+        {
+            if (time >= delay)
+            {
+                managers.serializableData.SaveGame("autosave_" + DateTime.Now);
+                time = 0.0f;
+            }
+            else
+            {
+                time += realTimeDelta;
+            }
+        }
+    }
+}
