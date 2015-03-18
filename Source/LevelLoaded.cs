b329170c4f3497f938a91bd28e9a3d34fa43f8e7
Palmer11 <jordie_796@hotmail.com>
2015-03-18 13:10:28 -0400

    Resync

---
+++ Source/LevelLoaded.cs
@@ -0,0 +1,51 @@
+﻿/// Created By: Palmer11
+/// Steam ID: oPalmer
+using ICities;
+using UnityEngine;
+using Handler.Save;
+using ColossalFramework.IO;
+
+namespace Mod.AutoSave
+{
+    public class LevelLoaded : LoadingExtensionBase
+    {
+        private const string CONST_FILE_CONFIG = @"\..\autosave_config.xml";
+        private static AutoSaveHandler m_AutoSaveHandler = null;
+        public static SaveHandler m_SaveHandler = null;
+
+        public override void OnLevelLoaded(LoadMode mode)
+        {
+            m_SaveHandler = new SaveHandler();
+
+            string sl = DataLocation.saveLocation + CONST_FILE_CONFIG;
+            m_SaveHandler.CreateFile<ConfigFile>(sl, new ConfigFile(), false);
+            AutoSaveHandler.m_ConfigFile = m_SaveHandler.GetFile<ConfigFile>(sl);
+
+            short td = AutoSaveHandler.m_ConfigFile.TimeDelay;
+            if (TAutoSave.m_Delay != td)
+            {
+                if (td > AutoSaveHandler.CONST_MAX_FILE_COUNT)
+                {
+                    td = AutoSaveHandler.CONST_MAX_FILE_COUNT;
+                }
+
+                TAutoSave.m_Delay = td;
+            }
+
+            m_AutoSaveHandler = new AutoSaveHandler();
+        }
+
+        public override void OnLevelUnloading()
+        {
+            if (m_AutoSaveHandler != null)
+            {
+                m_AutoSaveHandler = null;
+            }
+
+            if (m_SaveHandler != null)
+            {
+                m_SaveHandler = null;
+            }
+        }
+    }
+}
---
+++ Source/TAutoSave.cs
@@ -0,0 +1,32 @@
+﻿/// Created By: Palmer11
+/// Steam ID: oPalmer
+using System;
+using ICities;
+
+namespace Mod.AutoSave
+{
+    public class TAutoSave : ThreadingExtensionBase
+    {
+       
+        private static float m_Time = 0.0f;
+        public static float m_Delay = 600.0f;
+
+        public override void OnUpdate(float realTimeDelta, float simulationTimeDelta)
+        {
+            if (m_Time >= m_Delay)
+            {
+                managers.serializableData.SaveGame(AutoSaveHandler.GetFileName());
+                m_Time = 0.0f;
+            }
+            else
+            {
+                m_Time += realTimeDelta;
+            }
+        }
+
+        //public static void DebugMsg(string msg)
+        //{
+        //    DebugOutputPanel.AddMessage(ColossalFramework.Plugins.PluginManager.MessageType.Message, msg);
+        //}
+    }
+}
